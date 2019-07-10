99e3076bc55f2df3f86bd290303e95b43a1a66ff
Kariuki62 <kariukiwan62@gmail.com>
2019-07-10 12:56:12 -0400

    Added a Departments Dao file and made changes to it.

---
+++ src/main/java/dao/DepartmentsDao.java
@@ -0,0 +1,25 @@
+package dao;
+
+
+import models.Departments;
+import models.News;
+
+import java.util.List;
+
+public interface DepartmentsDao {
+    //create
+    void add (Departments department);
+    void addDepartmentToNews(Departments department, News news);
+
+    //read
+    List<Departments> getAll();
+    Departments findById(int id);
+    List<News> getAllNewsByDepartment(int departmentId);
+
+    //update
+    void update(int id, String name, String description, int numberofemployees);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+}
